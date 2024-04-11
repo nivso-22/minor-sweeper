6efd6e781a59c351dccea10eb94bd760d048d6ed
nivso-22 <122080004+nivso-22@users.noreply.github.com>
2024-04-11 22:51:28 +0300

    Add files via upload

--- print the thingy.py
+++ print the thingy.py
@@ -4,8 +4,8 @@ import random
 for i in range(20):
     print("dw", end=" ")
     for j in range(20):
-        if random.randint(0, 20) == 16:
-            print("0dh", end= ", ")
+        if random.randint(0, 40) == 38:
+            print("0f000h", end= ", ")
             continue
-        print("0ff00h", end=", ")
+        print("0ff00h", end=" " if j==19 else ", ")
     print()
